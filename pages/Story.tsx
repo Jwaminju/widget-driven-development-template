19afd024a7266b70f6612e04622ee5d122fce14f
hojooneum <bldolphin96@gmail.com>
2023-03-03 20:11:15 +0900

    feat: add story box and story text to Story page

---
+++ pages/Story.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next'
+import styles from "../styles/Story.module.css";
+import Head from "next/head";
+const STORY_TXT = `Global warming is an ongoing environmental crisis that has been intensifying over the past few decades. 
+It refers to the long-term increase in the Earth's average surface temperature, caused primarily by the build-up of 
+greenhouse gases in the atmosphere, such as carbon dioxide and methane. 
+The consequences of global warming include rising sea levels, more frequent and severe natural disasters, 
+loss of biodiversity, and adverse impacts on human health and livelihoods. 
+Despite international efforts to mitigate global warming, such as the Paris Agreement, 
+the concentration of greenhouse gases continues to rise, and urgent action is needed to avoid catastrophic outcomes.`;
+const Story: NextPage = () => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>CarbonHero</title>
+                <meta name="description" content="Education game for Global Warming" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/*main 요소 백그라운드에 이미지 정도 넣으면 되지 않을까 싶어요.*/}
+            <main className={styles.main}>
+                <div className={styles.story_box}>
+                    <p className={styles.story_txt}>
+                        {STORY_TXT}
+                    </p>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default Story
