381fc559f10dbf26d610fb183d202d2bed4d85c4
hojooneum <bldolphin96@gmail.com>
2023-03-14 20:08:43 +0900

    feat(component): add styles to heading1

--- components/Heading1/index.tsx
+++ components/Heading1/index.tsx
@@ -1,16 +1,16 @@
-import {Heading} from "@chakra-ui/react";
+import {Heading, Highlight} from "@chakra-ui/react";
 
 interface Heading1Props {
-    label: string;
+    label?: string;
     size?: string;
 }
 
 const Heading1 = ({
-  label,
-  size = 'xl'
+  label = "Heading1",
+  size = '4xl'
                   }: Heading1Props) => {
     return(
-      <Heading as='h1' size={size}>
+      <Heading as='h1' size={size} fontSize={'4rem'} fontWeight={'bold'}>
         {label}
       </Heading>
     );
