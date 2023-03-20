a0476bc641670cf8f66c56af75f529a118393cd6
hojooneum <bldolphin96@gmail.com>
2023-03-21 00:45:02 +0900

    feat(model): add GreenHouseGas models

--- models/greenhousegas.ts
+++ models/greenhousegas.ts
@@ -1,4 +1,51 @@
 export interface GreenHouseGas {
-  getEffect: () => number;
-  setEffect: (effect: number) => void;
+  get name(): string;
+  get measurement(): number;
+  set measurement(measurement: number);
+}
+
+export class Co2 implements GreenHouseGas {
+  measurement: number = 0.0;
+  name: string = "Co2";
+  constructor(measurement: number) {
+    this.measurement = measurement;
+  }
+}
+
+export class No2 implements GreenHouseGas {
+  measurement: number = 0.0;
+  name: string = "No2";
+  constructor(measurement: number) {
+    this.measurement = measurement;
+  }
+}
+
+export class Ch4 implements GreenHouseGas {
+  measurement: number = 0.0;
+  name: string = "Ch4";
+  constructor(measurement: number) {
+    this.measurement = measurement;
+  }
+}
+
+export class H2o implements GreenHouseGas {
+  measurement: number = 0.0;
+  name: string = "H2o";
+  constructor(measurement: number) {
+    this.measurement = measurement;
+  }
+}
+export class GasFactory {
+  public createGas(type: string, measurement: number): GreenHouseGas {
+    switch (type) {
+      case "co2":
+        return new Co2(measurement);
+      case "no2":
+        return new No2(measurement);
+      case "ch4":
+        return new Ch4(measurement);
+      default:
+        return new H2o(measurement);
+    }
+  }
 }
