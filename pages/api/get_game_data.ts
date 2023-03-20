49fa94523f3eb6af45396004a934757996026e41
hojooneum <bldolphin96@gmail.com>
2023-03-21 00:45:34 +0900

    feat(data): add game data json file for mockup

--- pages/api/get_game_data.ts
+++ pages/api/get_game_data.ts
@@ -1,13 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import game_data from "../../public/game_data.json";
+import {GameStateData} from "../../hooks/useGameState";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<GameStateData>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(game_data);
 }
