bf0ec425f0f5112ef598d842c280075cccb074c9
hojooneum <bldolphin96@gmail.com>
2023-03-23 17:34:15 +0900

    chore(widget): remove unused hooks

--- widgets/storyScene/Presenter.tsx
+++ widgets/storyScene/Presenter.tsx
@@ -1,8 +1,11 @@
 import {Center} from "@chakra-ui/layout";
-import {Button, Container, Flex, SimpleGrid, Spacer, useDisclosure} from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody, CardFooter, Text
+} from "@chakra-ui/react";
 import Heading2 from "../../components/Heading2";
 import {STORY_TXT} from "../../data/main_story";
-import TextModalMultiPages from "../../components/TextModalMultiPages";
 import {User} from "firebase/auth";
 import LinkToNext from "../../components/LinkToNext";
 import AuthContainer from "../auth/Container";
@@ -18,28 +21,20 @@ const Presenter = ({
   user,
   labelForLinkToNext
                    }: Props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true, });
 
   return ( 
     <Center w={'100vw'} h={'100vh'} backgroundImage={'url(https://firebasestorage.googleapis.com/v0/b/green-5be28.appspot.com/o/globe-1849404.png?alt=media&token=11913eda-8200-4877-834d-f5622b989f4d)'} backgroundRepeat={'no-repeat'} backgroundPosition={'center'}>
-      <SimpleGrid spacing="10%"> 
-        <Spacer />
-        <Container>
-          <Flex>
-            <Button onClick={onOpen} opacity={0.5} p='5%'>
-              <Heading2 label={sceneTitle}/>
-            </Button>
-            <TextModalMultiPages 
-              textContents={STORY_TXT} 
-              initOpen={true}
-              isOpen={isOpen} 
-              onClose={onClose}/>
-            <Spacer />
-          </Flex>
-          {user ? <LinkToNext label={"Play Game"} nextPageRoute={"main"} /> : <AuthContainer />}
-        </Container>
-        <Spacer />
-      </SimpleGrid>
+      <Card w={"50%"} boxShadow='xs' p='6' rounded='md' bg='white'>
+        <CardHeader>
+          <Heading2 label={"Story is..."} />
+        </CardHeader>
+        <CardBody>
+          <Text>{STORY_TXT}</Text>
+        </CardBody>
+        <CardFooter>
+          {user ? <LinkToNext label={"Play Game"} nextPageRoute={labelForLinkToNext} /> : <AuthContainer />}
+        </CardFooter>
+      </Card>
     </Center>
     );
 }
