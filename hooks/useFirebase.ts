5e207cc08b4399b0620c171acd08af5b50f2ecd0
tmddus2 <tmddus2__@naver.com>
2023-03-25 10:30:13 -0700

    feat: add saving data hook

--- hooks/useFirebase.ts
+++ hooks/useFirebase.ts
@@ -1,27 +1,37 @@
-import {useAuthState, useSignInWithGoogle, useSignOut} from "react-firebase-hooks/auth";
-import {initializeApp} from "firebase/app";
-import {firebaseConfig} from "../configs/firebase.config";
-import {getAuth} from "firebase/auth";
+import { useAuthState, useSignInWithGoogle, useSignOut } from "react-firebase-hooks/auth";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../configs/firebase.config";
+import { getAuth } from "firebase/auth";
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import { GameState } from "./useGameState";
+import { getFirestore, collection, getDocs, doc, getDoc, query } from 'firebase/firestore';
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
+const database = getDatabase(firebaseApp);
+const db = getFirestore(firebaseApp);
 export const useFirebaseSignIn = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   const signIn = () => {
     return signInWithGoogle();
   }
-  return {signIn, user, loading, error};
+  return { signIn, user, loading, error };
 }
 
 export const useFirebaseSignOut = () => {
   const [signOut, loading, error] = useSignOut(auth);
-  return {signOut, loading, error};
+  return { signOut, loading, error };
 }
 
 export const useFirebaseAuthState = () => {
   const [user, loading, error] = useAuthState(auth);
-  return {user, loading, error};
+  return { user, loading, error };
 }
+
+export const useFirebaseSaveData = (gamestage: GameState) => {
+  return set(ref(database, 'gamedatas/' + gamestage.id), gamestage);
+}
+
 
 
 
