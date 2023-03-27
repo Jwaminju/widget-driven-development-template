c3aa472424319f758229bbede222325ae692c62a
hojooneum <bldolphin96@gmail.com>
2023-03-27 19:05:38 +0900

    chore(data): remove unused data

--- pages/api/get_game_data.ts
+++ pages/api/get_game_data.ts
@@ -1,11 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
-import game_data from "../../public/game_data.json";
 import {GameStateData} from "../../hooks/useGameState";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<GameStateData>
 ) {
-  res.status(200).json(game_data);
+  res.status(200);
 }
