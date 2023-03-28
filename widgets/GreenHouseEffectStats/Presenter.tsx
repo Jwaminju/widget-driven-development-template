b57a781dce10d2b23b7c8a9c8507220b055464b5
hojooneum <bldolphin96@gmail.com>
2023-03-29 00:29:23 +0900

    feat(widget): add divider to stat group

--- widgets/GreenHouseEffectStats/Presenter.tsx
+++ widgets/GreenHouseEffectStats/Presenter.tsx
@@ -1,6 +1,7 @@
 import {StatGroup} from "@chakra-ui/stat";
 import StatIndicator from "../../components/StatIndicator";
 import {defaultGreenHouseGases, GreenHouseGas} from "../../models/greenhousegas";
+import {Divider} from "@chakra-ui/react";
 
 interface Props {
   greenHouseEffect: number | null;
@@ -27,9 +28,13 @@ const Presenter = ({
         padding={5}
       >
           <StatIndicator label={"GreenHouseEffect"} statNumber={greenHouseEffect||0} changeRate={changeRates?.get("greenHouseEffect")||0} />
+          <Divider orientation={"vertical"} />
           <StatIndicator label={"Co2"} statNumber={co2.concentration} changeRate={changeRates?.get("co2")||0} />
+          <Divider orientation={"vertical"} />
           <StatIndicator label={"N2o"} statNumber={n2o.concentration} changeRate={changeRates?.get("n2o")||0} />
+          <Divider orientation={"vertical"} />
           <StatIndicator label={"Ch4"} statNumber={ch4.concentration} changeRate={changeRates?.get("ch4")||0} />
+          <Divider orientation={"vertical"} />
           <StatIndicator label={"Cfcs"} statNumber={cfcs.concentration} changeRate={changeRates?.get("cfcs")||0} />
       </StatGroup>
     );
