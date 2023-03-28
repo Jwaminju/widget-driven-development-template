2e6964d41afeaa497d1c9ba68dd9de52ec3616e8
hojooneum <bldolphin96@gmail.com>
2023-03-29 00:05:28 +0900

    feat(theme): add custom theme for stats

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,13 +1,11 @@
 import type {NextPage} from 'next'
 import Head from "next/head";
 import {ChakraProvider} from "@chakra-ui/provider";
-import {RecoilRoot} from "recoil";
-import {GameStateData, useGameState} from "../hooks/useGameState";
 import GlobeContainer from "../widgets/globe/Container";
 import GreenHouseEffectStatsContainer from "../widgets/GreenHouseEffectStats/Container";
+import {customTheme} from "../theme/theme";
 
-const Main: NextPage<any> = ({ gameState }) => {
-    // const { currentGameState, userInfo, playTime, greenHouseGases, items } = useGameState(gameState);
+const Main: NextPage<any> = () => {
     return (
         <>
           <Head>
@@ -15,19 +13,12 @@ const Main: NextPage<any> = ({ gameState }) => {
               <meta name="description" content="Education game for Global Warming" />
               <link rel="icon" href="/favicon.ico" />
           </Head>
-          <ChakraProvider>
-              <RecoilRoot>
-                <GreenHouseEffectStatsContainer />
-                <GlobeContainer />
-              </RecoilRoot>
+          <ChakraProvider theme={customTheme}>
+            <GreenHouseEffectStatsContainer />
+            <GlobeContainer />
           </ChakraProvider>
         </>
     );
 }
-
-// Main.getInitialProps = async (context) => {
-//   const gameState: GameStateData = game_data;
-//   return { gameState };
-// }
 
 export default Main
