2134cd281c8d1577ff62b67d61db7285c7d47421
wony617 <49024958+Jwaminju@users.noreply.github.com>
2023-04-01 16:34:18 +0900

    fix: the GameOverSign import

--- components/CommonButton/index.tsx
+++ components/CommonButton/index.tsx
@@ -2,11 +2,7 @@ import { Button } from "@chakra-ui/react";
 
 interface Props {
     label: string;
-<<<<<<< HEAD
-    onClick: () => any;
-=======
     onClick: React.MouseEventHandler<HTMLButtonElement>;
->>>>>>> 77485259 (fix: the CommonButton Type error)
 }
 
 const CommonButton = ({label, onClick}: Props) => {
